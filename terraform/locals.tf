04c92dbed0916fb532d97a2a02fcd9813d43541a
Udi Dahan <udid@amazon.com>
2022-12-08 06:40:22 +0000

    - Added "k8s_namespace" local variable, to be used in the "assume_role_policy" part of the service account IAM Role - Added "k8s_service_account" local variable, to be used in the "assume_role_policy" part of the service account IAM Role - Added "eks_oidc_url" local variable, to be used in the "assume_role_policy" part of the service account IAM Role

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,7 @@
 locals {
   name = "kubecost_cid"
   bucket = "kubecost-cid"
+  k8s_namespace = "kubecost-cid"
+  k8s_service_account = "kubecost-cid2"
+  eks_oidc_url = "arn:aws:iam::742719403826:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/842C756C29C13E7A449FA13B06A228BB"
 }
