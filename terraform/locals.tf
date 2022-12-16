63528392bf98043ad99a9edeae6c9034319d0833
Udi Dahan <udid@amazon.com>
2022-12-16 15:41:09 +0000

    - Split AWS resources locals and K8s resources locals - Added labels local - Changed default values of locals

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,13 @@
+# AWS resources
 locals {
-  name = "kubecost_cid"
-  bucket = "kubecost-cid"
-  k8s_namespace = "kubecost-cid"
-  k8s_service_account = "kubecost-cid2"
+  name         = "kubecost-cid"
+  bucket       = "kubecost-cid"
   eks_oidc_url = "arn:aws:iam::742719403826:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/842C756C29C13E7A449FA13B06A228BB"
+}
+
+# K8s resources referenced in AWS resources
+locals {
+  k8s_namespace       = local.name
+  k8s_service_account = local.name
+  k8s_labels          = ["app", "chart"]
 }
