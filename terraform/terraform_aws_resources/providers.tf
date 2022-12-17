91c77abf6aed94d166d89f8bb211e0d766f65363
Udi Dahan <udid@amazon.com>
2022-12-17 18:05:14 +0000

    - Removed "terraform" key (that had "required_providers" and "required_version" keys)

--- terraform/terraform_aws_resources/providers.tf
+++ terraform/terraform_aws_resources/providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-east-1"
+  region = local.region
 }
