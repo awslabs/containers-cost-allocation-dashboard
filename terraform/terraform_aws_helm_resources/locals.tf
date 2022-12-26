d86bb25d2d9140357b75370fcc91fa0aee995b42
Udi Dahan <udid@amazon.com>
2022-12-26 07:37:13 +0000

    - Renamed "cluster_id" local to "cluster_name" - Changed "image" and "cluster_name" locals values to be placeholders

--- terraform/terraform_aws_helm_resources/locals.tf
+++ terraform/terraform_aws_helm_resources/locals.tf
@@ -10,17 +10,17 @@ locals {
   bucket_arn           = "<arn>"
   k8s_config_path      = "~/.kube/config"
   k8s_namespace        = "${local.name}-kubecost-s3-exporter"
-  k8s_create_namespace = true
   k8s_service_account  = "${local.name}-kubecost-s3-exporter"
+  k8s_create_namespace = true
 }
 
 # Kubecost S3 Exporter locals
 locals {
-  image                 = "udid/kubecost_cid:0.1.0"
+  image                 = "<your_registry_url>/<your_repo>:<tag>"
   image_pull_policy     = "Always"
   schedule              = "@midnight"                                   # The kubecost-s3-exporter pod schedule, in cron format (UTC)
   kubecost_api_endpoint = "http://kubecost-cost-analyzer.kubecost:9090" # Change to your Kubecost endpoint if necessary
-  cluster_id            = "cluster-one"                                 # Change to your EKS cluster name
+  cluster_name          = "<cluster_name>"                              # Your EKS cluster name
   granularity           = "hourly"
   k8s_labels            = [] # Example: ["app", "chart"]
 }
