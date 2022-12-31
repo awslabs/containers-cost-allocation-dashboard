948d67b46211f2f5eb881d9166f2da08264e75da
Udi Dahan <udid@amazon.com>
2022-12-31 17:33:48 +0000

    - Removed "name" input - Changed Cron schdule - Changed naming convention - Separated inputs to "Required" and "Optional", for clarity

--- terraform/terraform_aws_resources/locals.tf
+++ terraform/terraform_aws_resources/locals.tf
@@ -1,14 +1,13 @@
-# General locals
+# Required Inputs
 locals {
-  name = "my_name" # This name will be prepended to different resources names in AWS and in K8s
+  region       = "<region>"   # Example: "us-east-1"
+  eks_oidc_url = "<oidc_url>" # Example: "arn:aws:iam::<account_id>:oidc-provider/oidc.eks.<region>.amazonaws.com/id/<id>
+  bucket_arn   = "<arn>"
 }
 
-# AWS resources, and K8s resources referenced in AWS resources
+# Optional Inputs
 locals {
-  region              = "<region>"   # Example: "us-east-1"
-  eks_oidc_url        = "<oidc_url>" # Example: "arn:aws:iam::<account_id>:oidc-provider/oidc.eks.<region>.amazonaws.com/id/<id>
-  bucket_arn          = "<arn>"
-  k8s_namespace       = "${local.name}-kubecost-s3-exporter"
-  k8s_service_account = "${local.name}-kubecost-s3-exporter"
+  k8s_namespace       = "kubecost-s3-exporter"
+  k8s_service_account = "kubecost-s3-exporter"
   k8s_labels          = [] # Example: ["app", "chart"]
 }
