c172768bbd4dd356b99ecc11b80f69af11db099f
Udi Dahan <udid@amazon.com>
2023-01-05 14:21:27 +0000

    - Changed the placeholder for "bucket_arn" input

--- terraform/terraform_aws_helm_resources/locals.tf
+++ terraform/terraform_aws_helm_resources/locals.tf
@@ -2,7 +2,7 @@
 locals {
   region                    = "<region>"                    # Example: "us-east-1"
   eks_iam_oidc_provider_arn = "<eks_iam_oidc_provider_arn>" # Example: "arn:aws:iam::<account_id>:oidc-provider/oidc.eks.<region>.amazonaws.com/id/<id>"
-  bucket_arn                = "<arn>"
+  bucket_arn                = "<bucket_arn>"
   image                     = "<registry_url>/<repo>:<tag>"
   cluster_name              = "<cluster_name>" # Your EKS cluster name
 }
