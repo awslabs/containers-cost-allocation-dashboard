f848648b2bc7621d4ef56c2d41aa9208c6a3b4a5
Udi Dahan <udid@amazon.com>
2023-01-27 14:29:12 +0000

    - Added AWS copyright notice

--- terraform/terraform_aws_resources/locals.tf
+++ terraform/terraform_aws_resources/locals.tf
@@ -1,3 +1,5 @@
+# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+
 # Required Inputs
 locals {
   region                    = "<region>"                    # Example: "us-east-1"
