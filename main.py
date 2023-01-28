fb3d39facb33419f6c1acc041b8ff02511516a70
Udi Dahan <udid@amazon.com>
2023-01-28 18:06:45 +0000

    - Removed unnecessary "import json"

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved.
 # This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
-import json
+
 import os
 import sys
 import gzip
