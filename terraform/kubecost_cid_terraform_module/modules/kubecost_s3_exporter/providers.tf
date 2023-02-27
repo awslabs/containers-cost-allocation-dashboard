4ba08e13ec73188320dd473a3f5e4a211511959a
Udi Dahan <udid@amazon.com>
2023-02-27 15:32:08 +0000

    - Initial providers.tf for the "kubecost_s3_exporter" module

---
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/providers.tf
@@ -0,0 +1,22 @@
+# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+
+provider "aws" {
+  region = var.aws_region
+  shared_config_files = module.common.aws_shared_config_files
+  shared_credentials_files = module.common.aws_shared_credentials_files
+  profile = var.aws_profile
+}
+
+provider "aws" {
+  alias = "irsa"
+  shared_config_files = module.common.aws_shared_config_files
+  shared_credentials_files = module.common.aws_shared_credentials_files
+  profile = module.common.irsa_aws_profile
+}
+
+provider "helm" {
+  kubernetes {
+    config_context = var.cluster_context
+    config_path = var.k8s_config_path
+  }
+}
