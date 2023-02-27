8250eb488d1381cac96136786809f50db0d5f76c
Udi Dahan <udid@amazon.com>
2023-02-27 15:33:14 +0000

    - Initial outputs.tf for the "kubecost_s3_exporter" module

---
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
@@ -0,0 +1,6 @@
+# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+
+output "irsa_iam_role_arn" {
+  value = aws_iam_role.kubecost_s3_exporter_service_account_role.arn
+  description = "The ARN of the IAM Role that was created as part of IRSA"
+}
