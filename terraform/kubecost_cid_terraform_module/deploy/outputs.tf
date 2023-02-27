42f8e478d585fe8530823f7f1ca4df0b67ababf1
Udi Dahan <udid@amazon.com>
2023-02-27 15:37:21 +0000

    - Initial outputs.tf for the deployed resources

---
+++ terraform/kubecost_cid_terraform_module/deploy/outputs.tf
@@ -0,0 +1,13 @@
+# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+
+# Output to show the distinct labels from all clusters, collected from the "clusters_labels" common input
+output "labels" {
+  value = module.pipeline.labels
+  description = "A list of the distinct lab of all clusters, that'll be added to the dataset"
+}
+
+# Outputs to show the IRSA IAM Role ARN that was created for each cluster
+output "cluster1_irsa_iam_role_arn" {
+  value = module.cluster1.irsa_iam_role_arn
+  description = "The IRSA IAM Role ARN for this cluster"
+}
