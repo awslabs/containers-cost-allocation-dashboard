0798073df1328ff2462d64ce23fcaac4512e0bb2
Udi Dahan <udid@amazon.com>
2023-03-04 19:21:14 +0000

    - Syntax fixes

--- terraform/kubecost_cid_terraform_module/examples/deploy/main.tf
+++ terraform/kubecost_cid_terraform_module/examples/deploy/main.tf
@@ -5,10 +5,10 @@
 ######################################
 
 module "pipeline" {
-  source   = "../modules/pipeline"
+  source = "../modules/pipeline"
 
-  aws_region = "us-east-1"
-  aws_profile = "pipeline_profile"
+  aws_region            = "us-east-1"
+  aws_profile           = "pipeline_profile"
   glue_crawler_schedule = "cron(0 1 * * ? *)"
 }
 
@@ -23,56 +23,56 @@ module "pipeline" {
 # Clusters in Region us-east-1 #
 
 module "cluster1-us-east-1-111111111111" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
-  cluster_context = "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/1"
-  aws_region = "us-east-1"
-  aws_profile = "profile1"
+  cluster_arn                          = "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
+  cluster_context                      = "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/1"
+  aws_region                           = "us-east-1"
+  aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  kubecost_api_endpoint = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
+  kubecost_api_endpoint                = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
 }
 
 module "cluster2-us-east-1-111111111111" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
-  cluster_context = "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/2"
-  aws_region = "us-east-1"
-  aws_profile = "profile1"
+  cluster_arn                          = "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
+  cluster_context                      = "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/2"
+  aws_region                           = "us-east-1"
+  aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  namespace = "kubecost-s3-exporter-2"
-  service_account = "kubecost-s3-exporter-2"
+  namespace                            = "kubecost-s3-exporter-2"
+  service_account                      = "kubecost-s3-exporter-2"
 }
 
 # Clusters in Region us-east-2 #
 
 module "cluster1-us-east-2-111111111111" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-2:111111111111:cluster/cluster1"
-  cluster_context = "context_cluster1_us-east-2_111111111111"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/3"
-  aws_region = "us-east-2"
-  aws_profile = "profile1"
-  kubecost_s3_exporter_container_image = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
+  cluster_arn                                      = "arn:aws:eks:us-east-2:111111111111:cluster/cluster1"
+  cluster_context                                  = "context_cluster1_us-east-2_111111111111"
+  cluster_oidc_provider_arn                        = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/3"
+  aws_region                                       = "us-east-2"
+  aws_profile                                      = "profile1"
+  kubecost_s3_exporter_container_image             = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
   kubecost_s3_exporter_container_image_pull_policy = "IfNotPresent"
-  kubecost_s3_exporter_pod_schedule = "0 0 * * 5"
-  kubecost_api_endpoint = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
+  kubecost_s3_exporter_pod_schedule                = "0 0 * * 5"
+  kubecost_api_endpoint                            = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
 }
 
 module "cluster2-us-east-2-111111111111" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-2:111111111111:cluster/cluster2"
-  cluster_context = "context_cluster2_us-east-2_111111111111"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/4"
-  aws_region = "us-east-2"
-  aws_profile = "profile1"
+  cluster_arn                          = "arn:aws:eks:us-east-2:111111111111:cluster/cluster2"
+  cluster_context                      = "context_cluster2_us-east-2_111111111111"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/4"
+  aws_region                           = "us-east-2"
+  aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  invoke_helm = false
+  invoke_helm                          = false
 }
 
 #                                  #
@@ -82,55 +82,55 @@ module "cluster2-us-east-2-111111111111" {
 # Clusters in Region us-east-1 #
 
 module "cluster1-us-east-1-222222222222" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
-  cluster_context = "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5"
-  aws_region = "us-east-1"
-  aws_profile = "profile2"
+  cluster_arn                          = "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
+  cluster_context                      = "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5"
+  aws_region                           = "us-east-1"
+  aws_profile                          = "profile2"
   kubecost_s3_exporter_container_image = "222222222222.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  kubecost_api_endpoint = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
-  k8s_config_path = "~/configs/k8s/config"
+  kubecost_api_endpoint                = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
+  k8s_config_path                      = "~/configs/k8s/config"
 }
 
 module "cluster2-us-east-1-222222222222" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
-  cluster_context = "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/6"
-  aws_region = "us-east-1"
-  aws_profile = "profile1"
+  cluster_arn                          = "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
+  cluster_context                      = "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/6"
+  aws_region                           = "us-east-1"
+  aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "222222222222.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
 }
 
 # Clusters in Region us-east-2 #
 
 module "cluster1-us-east-2-222222222222" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-2:222222222222:cluster/cluster1"
-  cluster_context = "context_cluster1_us-east-2_222222222222"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/7"
-  aws_region = "us-east-2"
-  aws_profile = "profile2"
+  cluster_arn                          = "arn:aws:eks:us-east-2:222222222222:cluster/cluster1"
+  cluster_context                      = "context_cluster1_us-east-2_222222222222"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/7"
+  aws_region                           = "us-east-2"
+  aws_profile                          = "profile2"
   kubecost_s3_exporter_container_image = "222222222222.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  kubecost_api_endpoint = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
-  invoke_helm = false
+  kubecost_api_endpoint                = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
+  invoke_helm                          = false
 }
 
 module "cluster2-us-east-2-222222222222" {
-  source   = "../modules/helm"
+  source = "../modules/helm"
 
-  cluster_arn = "arn:aws:eks:us-east-2:222222222222:cluster/cluster2"
-  cluster_context = "context_cluster2_us-east-2_222222222222"
-  cluster_oidc_provider_arn = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/8"
-  aws_region = "us-east-2"
-  aws_profile = "profile1"
+  cluster_arn                          = "arn:aws:eks:us-east-2:222222222222:cluster/cluster2"
+  cluster_context                      = "context_cluster2_us-east-2_222222222222"
+  cluster_oidc_provider_arn            = "arn:aws:iam::333333333333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/8"
+  aws_region                           = "us-east-2"
+  aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "222222222222.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  namespace = "kubecost-s3-exporter-2"
-  create_namespace = false
-  service_account = "kubecost-s3-exporter-2"
-  create_service_account = false
+  namespace                            = "kubecost-s3-exporter-2"
+  create_namespace                     = false
+  service_account                      = "kubecost-s3-exporter-2"
+  create_service_account               = false
 }
