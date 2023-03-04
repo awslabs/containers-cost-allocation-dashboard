d5d7532670e3fe51a9c57e18d20ae885d2d0c0fe
Udi Dahan <udid@amazon.com>
2023-03-04 19:24:35 +0000

    - Syntax fixes

--- terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
@@ -1,16 +1,16 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "aws_region" {
-  type = string
+  type        = string
   description = "The AWS region code to use for the pipeline resources"
 }
 
 variable "aws_profile" {
-  type = string
+  type        = string
   description = "The AWS profile to use for configuration and credentials to create the pipeline resources"
 }
 
 variable "glue_crawler_schedule" {
-  type = string
+  type        = string
   description = "The schedule for the Glue Crawler, in Cron format. Make sure to set it after the last Kubecost S3 Exporter Cron schedule"
 }
