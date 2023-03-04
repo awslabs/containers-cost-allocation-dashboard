8dda290b7b73a36926058a22d3e11303ea246456
Udi Dahan <udid@amazon.com>
2023-03-04 19:24:55 +0000

    - Syntax fixes

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -1,6 +1,6 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "labels" {
-  value = local.labels_for_output
+  value       = local.labels_for_output
   description = "List of the distinct labels from all clusters"
 }
