2d5f1108651deb6703d1234b4ed5d6b2bf10b221
Udi Dahan <udid@amazon.com>
2023-03-04 19:24:18 +0000

    - Syntax fixes

--- terraform/kubecost_cid_terraform_module/modules/pipeline/providers.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/providers.tf
@@ -1,8 +1,8 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 provider "aws" {
-  region = var.aws_region
-  shared_config_files = module.common.aws_shared_config_files
+  region                   = var.aws_region
+  shared_config_files      = module.common.aws_shared_config_files
   shared_credentials_files = module.common.aws_shared_credentials_files
-  profile = var.aws_profile
+  profile                  = var.aws_profile
 }
