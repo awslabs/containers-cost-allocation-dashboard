d80c31996d2b1997e38dbf04841703391af5be14
Udi Dahan <udid@amazon.com>
2023-03-04 19:22:33 +0000

    Replace variables.tf

--- terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
+++ terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
@@ -1,14 +1,14 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "bucket_arn" {
-  type = string
-  default = "arn:aws:s3:::kubecost-data-collection-bucket"
+  type        = string
+  default     = "arn:aws:s3:::kubecost-data-collection-bucket"
   description = "The ARN of the S3 Bucket to which the Kubecost data will be uploaded"
 }
 
 variable "irsa_aws_profile" {
-  type = string
-  default = "s3_bucket_account_aws_profile"
+  type        = string
+  default     = "s3_bucket_account_aws_profile"
   description = "The AWS profile to use for configuration and credentials to create the IRSA in the S3 bucket's account"
 }
 
@@ -16,33 +16,33 @@ variable "clusters_labels" {
 
   type = list(object({
     cluster_arn = string
-    labels = optional(list(string))
+    labels      = optional(list(string))
   }))
 
   default = [
     {
-      "cluster_arn": "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
-      "labels": ["app", "chart"]
+      "cluster_arn" : "arn:aws:eks:us-east-1:111111111111:cluster/cluster1"
+      "labels" : ["app", "chart"]
     },
     {
-      "cluster_arn": "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
-      "labels": ["app", "chart"]
+      "cluster_arn" : "arn:aws:eks:us-east-1:111111111111:cluster/cluster2"
+      "labels" : ["app", "chart"]
     },
     {
-      "cluster_arn": "arn:aws:eks:us-east-2:111111111111:cluster/cluster1"
-      "labels": ["app", "chart", "env"]
+      "cluster_arn" : "arn:aws:eks:us-east-2:111111111111:cluster/cluster1"
+      "labels" : ["app", "chart", "env"]
     },
     {
-      "cluster_arn": "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
-      "labels": ["app", "chart", "owner", "environment"]
+      "cluster_arn" : "arn:aws:eks:us-east-1:222222222222:cluster/cluster1"
+      "labels" : ["app", "chart", "owner", "environment"]
     },
     {
-      "cluster_arn": "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
-      "labels": ["app", "chart", "owner"]
+      "cluster_arn" : "arn:aws:eks:us-east-1:222222222222:cluster/cluster2"
+      "labels" : ["app", "chart", "owner"]
     },
     {
-      "cluster_arn": "arn:aws:eks:us-east-2:222222222222:cluster/cluster2"
-      "labels": ["app", "chart", "owner"]
+      "cluster_arn" : "arn:aws:eks:us-east-2:222222222222:cluster/cluster2"
+      "labels" : ["app", "chart", "owner"]
     }
   ]
 
@@ -50,19 +50,19 @@ variable "clusters_labels" {
 }
 
 variable "aws_shared_config_files" {
-  type = list(string)
-  default = ["~/.aws/config"]
+  type        = list(string)
+  default     = ["~/.aws/config"]
   description = "Paths to the AWS shared config files"
 }
 
 variable "aws_shared_credentials_files" {
-  type = list(string)
-  default = ["~/.aws/credentials"]
+  type        = list(string)
+  default     = ["~/.aws/credentials"]
   description = "Paths to the AWS shared credentials files"
 }
 
 variable "granularity" {
-  type = string
-  default = "hourly"
+  type        = string
+  default     = "hourly"
   description = "The time granularity of the data that is returned from the Kubecost Allocation API"
 }
