3b3161fde99cc4c1d376afbb2fd9785c7812f748
Udi Dahan <udid@amazon.com>
2023-03-04 19:23:29 +0000

    - Syntax fixes

--- terraform/kubecost_cid_terraform_module/modules/common/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/common/variables.tf
@@ -1,14 +1,14 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "bucket_arn" {
-  type = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The ARN of the S3 Bucket to which the Kubecost data will be uploaded"
 }
 
 variable "irsa_aws_profile" {
-  type = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The AWS profile to use for configuration and credentials to create the IRSA in the S3 bucket's account"
 }
 
@@ -16,7 +16,7 @@ variable "clusters_labels" {
 
   type = list(object({
     cluster_arn = string
-    labels = optional(list(string))
+    labels      = optional(list(string))
   }))
 
   default = []
@@ -25,19 +25,19 @@ variable "clusters_labels" {
 }
 
 variable "aws_shared_config_files" {
-  type = list(string)
-  default = ["~/.aws/config"]
+  type        = list(string)
+  default     = ["~/.aws/config"]
   description = "Paths to the AWS shared config files"
 }
 
 variable "aws_shared_credentials_files" {
-  type = list(string)
-  default = ["~/.aws/credentials"]
+  type        = list(string)
+  default     = ["~/.aws/credentials"]
   description = "Paths to the AWS shared credentials files"
 }
 
 variable "granularity" {
-  type = string
-  default = "hourly"
+  type        = string
+  default     = "hourly"
   description = "The time granularity of the data that is returned from the Kubecost Allocation API"
 }
