a7f4b8453281bce409e4a2510413289b8f2a85ff
Udi Dahan <udid@amazon.com>
2023-03-06 07:51:27 +0000

    - Changed the input validations for "bucket_arn" and "cluster_arn" to be more simplified - Added more comments to explain the validations

--- terraform/kubecost_cid_terraform_module/modules/common/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/common/variables.tf
@@ -5,43 +5,24 @@ variable "bucket_arn" {
   default     = ""
   description = "The ARN of the S3 Bucket to which the Kubecost data will be uploaded"
 
-  # Use-cases for the below input validation:
-  # The "bucket_arn" input is empty ("")
-  # The "bucket_arn" input has some unstructured string ("test")
-  # The "bucket_arn" input has less than 6 fields ("arn:aws:s3")
+  # The below validation validates the "bucket_arn" input.
+  # It'll return the specified error if the input fails the validation.
+  # Here are a few examples of a "bucket_arn" input that'll fail validation:
+  # The "bucket_arn" key is empty ("")
+  # The "bucket_arn" key has some unstructured string (e.g. "test")
+  # The "bucket_arn" key has less than 6 ARN fields ("arn:aws:eks")
+  # The bucket name part of the ARN doesn't match the bucket name rules
+  # The "bucket_arn" input has missing ARN fields, has value in fields where there shouldn't be, or has incorrect value in some ARN fields. A few examples:
+  # arn:aws:s3:us-east-1:111111111111:bucket_name
+  # arn:aws:eks:::bucket_name
+  # arn:aaa:s3:::bucket_name
+  #
+  # Note - the full regex should have been "^arn:(?:aws|aws-cn|aws-us-gov):s3:::(?!(xn--|.+-s3alias$))[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$"
+  # The "(?!(xn--|.+-s3alias$))" part has been omitted because Terraform regex engine doesn't support negative lookahead (the "?!" part)
+  # Therefore, it has been removed, and instead, "!startswith" and "!endswith" conditions have been added, to complement this missing functionality
   validation {
-    condition = length(split(":", var.bucket_arn)) == 6
+    condition = can(regex("^arn:(?:aws|aws-cn|aws-us-gov):s3:::[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.bucket_arn)) && !startswith(element(split(":::", var.bucket_arn), 1), "xn--") && !endswith(element(split(":::", var.bucket_arn), 1), "-s3alias")
     error_message = "The 'bucket_arn' input contains an invalid ARN"
-  }
-
-  # Example of an invalid input for the below validation: arn:aaa:s3:::bucket1
-  validation {
-    condition = contains(["aws", "aws-cn", "aws-us-gov"], element(split(":", var.bucket_arn), 1))
-    error_message = "The 'bucket_arn' input includes an invalid partition field. The ARN partition field should be one of 'aws', 'aws-cn' or 'aws-us-gov'"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:eks:::bucket1
-  validation {
-    condition = element(split(":", var.bucket_arn), 2) == "s3"
-    error_message = "The 'bucket_arn' input includes an invalid service field. The ARN service field should be 's3'"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1::bucket1
-  validation {
-    condition = element(split(":", var.bucket_arn), 3) == ""
-    error_message = "The 'bucket_arn' input includes a region, but the region field for an S3 bucket ARN must be empty"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3::111111111111:bucket1
-  validation {
-    condition = element(split(":", var.bucket_arn), 4) == ""
-    error_message = "The 'bucket_arn' input includes an account ID, but the account ID field for an S3 bucket ARN must be empty"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:::
-  validation {
-    condition = element(split(":", var.bucket_arn), 5) != ""
-    error_message = "The 'bucket_arn' input's resource ID field is empty. It must contain the bucket name"
   }
 }
 
@@ -67,63 +48,25 @@ variable "clusters_labels" {
 
   description = "A map of clusters and their K8s labels that you wish to include in the dataset"
 
-  # Use-cases for the below input validation:
-  # The "cluster_arn" input is empty ("")
-  # The "cluster_arn" input has some unstructured string ("test")
-  # The "cluster_arn" input has less than 6 fields ("arn:aws:eks")
-  # The "cluster_arn" input has less than 6 fields, but is missing the "/" before the resource ID
-  # "cluster_arn" : "arn:aws:eks:us-east-1:111111111111:cluster"
+  # The below validation validates each "cluster_arn" key's value in each object in the "clusters_labels" list.
+  # It'll return the specified error if at least one of the "cluster_arn" keys' value fails the validation.
+  # Here are a few examples of a "cluster_arn" key's value that'll fail validation:
+  # The "cluster_arn" key's value is empty ("")
+  # The "cluster_arn" key's value has some unstructured string (e.g. "test")
+  # The "cluster_arn" key's value has less than 6 ARN fields ("arn:aws:eks")
+  # The "cluster_arn" key's value has 6 ARN fields, but is missing the "/" before the resource ID ("arn:aws:eks:us-east-1:111111111111:cluster")
+  # The EKS cluster name part of the ARN doesn't match the EKS cluster name rules
+  # The "cluster_arn" key's value has missing ARN fields or has incorrect value in some ARN fields. A few examples:
+  # arn:aws:eks:us-east-1:111111111111:cluster/
+  # arn:aws:eks:us-east-1:123:cluster/cluster1
+  # arn:aws:eks:us-east-1::cluster/cluster1
+  # arn:aws:eks:aaa:111111111111:cluster/cluster1
+  # arn:aws:eks::111111111111:cluster/cluster1
+  # arn:aws:s3:us-east-1:111111111111:cluster/cluster1
+  # arn:aaa:eks:us-east-1:111111111111:cluster/cluster1
   validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if length(split(":", cluster_arn)) == 6 && length(split("/", cluster_arn)) == 2]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, contains an invalid ARN"
-  }
-
-  # Example of an invalid input for the below validation: arn:aaa:eks:us-east-1:111111111111:cluster/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if contains(["aws", "aws-cn", "aws-us-gov"], element(split(":", cluster_arn), 1))]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, includes an invalid partition field. The ARN partition field should be one of 'aws', 'aws-cn' or 'aws-us-gov'"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1:111111111111:cluster/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if element(split(":", cluster_arn), 2) == "eks"]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, includes an invalid service field. The ARN service field should be 'eks'"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:eks::111111111111:cluster/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if element(split(":", cluster_arn), 3) != ""]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, is missing a region-code. The ARN of an EKS cluster must include a region-code"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:eks:us-east-1::cluster/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if element(split(":", cluster_arn), 4) != ""]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, is missing an account ID. The ARN of an EKS cluster must include an account ID"
-  }
-
-#  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1:aaa:cluster/cluster1
-#  validation {
-#    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if parseint(element(split(":", cluster_arn), 4), 10) == element(split(":", cluster_arn), 4)]) == length(var.clusters_labels)
-#    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, includes an account ID which is not a number. The AWS account ID must be a number"
-#  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1:11111111111:cluster/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if length(element(split(":", cluster_arn), 4)) == 12]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, includes an account ID with invalid length. The AWS account ID must be a 12-digit number"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1:111111111111:bucket/cluster1
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if element(split("/", element(split(":", cluster_arn), 5)), 0) == "cluster"]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, includes an invalid resource type. The ARN resource type field should be 'cluster'"
-  }
-
-  # Example of an invalid input for the below validation: arn:aws:s3:us-east-1:111111111111:cluster/
-  validation {
-    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if element(split("/", cluster_arn), 1) != ""]) == length(var.clusters_labels)
-    error_message = "One of the 'cluster_arn' inputs in the 'clusters_labels' list, is missing a resource ID. The ARN of an EKS cluster must include a resource ID"
+    condition = length([for cluster_arn in var.clusters_labels.*.cluster_arn : cluster_arn if can(regex("^arn:(?:aws|aws-cn|aws-us-gov):eks:(?:us(?:-gov)?|ap|ca|cn|eu|sa)-(?:central|(?:north|south)?(?:east|west)?)-\\d:\\d{12}:cluster\\/[a-zA-Z0-9-_]{1,100}$", cluster_arn))]) == length(var.clusters_labels)
+    error_message = "At least one of the 'cluster_arn' keys in the 'clusters_labels' list, contains an invalid ARN value"
   }
 }
 
@@ -132,7 +75,7 @@ variable "aws_shared_config_files" {
   default     = ["~/.aws/config"]
   description = "Paths to the AWS shared config files"
 
-  # Use-cases for the below input validation:
+  # Here are a few examples of a "aws_shared_config_files" input that'll fail validation:
   # The "aws_shared_config_files" has an empty list: []
   # The "aws_shared_config_files" has a single empty item: [""]
   validation {
@@ -146,7 +89,7 @@ variable "aws_shared_credentials_files" {
   default     = ["~/.aws/credentials"]
   description = "Paths to the AWS shared credentials files"
 
-  # Use-cases for the below input validation:
+  # Here are a few examples of a "aws_shared_credentials_files" input that'll fail validation:
   # The "aws_shared_credentials_files" has an empty list: []
   # The "aws_shared_credentials_files" has a single empty item: [""]
   validation {
