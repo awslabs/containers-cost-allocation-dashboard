f23786ecfb72c4e1923a93e8ee86551b644494d1
Udi Dahan <udid@amazon.com>
2023-03-06 14:21:00 +0000

    - Added input validations for "aws_profile" and "glue_crawler_schedule" inputs

--- terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
@@ -13,9 +13,19 @@ variable "aws_region" {
 variable "aws_profile" {
   type        = string
   description = "The AWS profile to use for configuration and credentials to create the pipeline resources"
+
+  validation {
+    condition = var.aws_profile != ""
+    error_message = "The 'aws_profile' input is empty. It must contain an AWS Profile name"
+  }
 }
 
 variable "glue_crawler_schedule" {
   type        = string
   description = "The schedule for the Glue Crawler, in Cron format. Make sure to set it after the last Kubecost S3 Exporter Cron schedule"
+
+  validation {
+    condition = var.glue_crawler_schedule != ""
+    error_message = "The 'glue_crawler_schedule' input is empty. It must contain a Cron expression"
+  }
 }
