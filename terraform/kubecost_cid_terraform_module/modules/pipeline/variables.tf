feae50b6581579742815ae1a4319ab14545f7861
Udi Dahan <udid@amazon.com>
2023-03-06 09:42:15 +0000

    - Added input validations - Changed "glue_crawler_schedule" default value to be only Cron expression, without the "cron()" part

--- terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
@@ -3,11 +3,21 @@
 variable "aws_region" {
   type        = string
   description = "The AWS region code to use for the pipeline resources"
+
+  validation {
+    condition = can(regex("(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\\d", var.aws_region))
+    error_message = "The 'aws_region' contains an invalid region-code"
+  }
 }
 
 variable "aws_profile" {
   type        = string
   description = "The AWS profile to use for configuration and credentials to create the pipeline resources"
+
+  validation {
+    condition = var.aws_profile != ""
+    error_message = "The 'aws_profile' input is empty. It must contain an AWS Profile name"
+  }
 }
 
 variable "glue_crawler_schedule" {
