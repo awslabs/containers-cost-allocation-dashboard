01944751ef001e2e23cd7c924f1d44837e25543d
Udi Dahan <udid@amazon.com>
2023-03-09 11:36:27 +0000

    - Changed the outputs to produce both IAM roles that are part of the IAM role chaining

--- terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
@@ -1,6 +1,11 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
-output "irsa_iam_role_arn" {
-  value       = aws_iam_role.kubecost_s3_exporter_service_account_role.arn
+output "irsa_child_iam_role_arn" {
+  value       = aws_iam_role.kubecost_s3_exporter_irsa_child_role.arn
   description = "The ARN of the IAM Role that was created as part of IRSA"
+}
+
+output "irsa_parent_iam_role_arn" {
+  value       = aws_iam_role.kubecost_s3_exporter_irsa_parent_role.arn
+  description = "The ARN of the parent IAM Role that was created as part of IRSA role chaining"
 }
