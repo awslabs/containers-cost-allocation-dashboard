7550100f3825dabe5f1677f8adf5d719d9b2640c
Udi Dahan <udid@amazon.com>
2023-04-12 20:24:15 +0000

    - Added example for tls_verify and for HTTPS Kubecost API enpodint

--- terraform/kubecost_cid_terraform_module/examples/deploy/main.tf
+++ terraform/kubecost_cid_terraform_module/examples/deploy/main.tf
@@ -31,9 +31,10 @@ module "cluster1-us-east-1-111111111111" {
   aws_region                           = "us-east-1"
   aws_profile                          = "profile1"
   kubecost_s3_exporter_container_image = "111111111111.dkr.ecr.us-east-1.amazonaws.com/kubecost_cid:0.1.0"
-  kubecost_api_endpoint                = "http://kubecost-eks-cost-analyzer.kubecost-eks:9090"
+  kubecost_api_endpoint                = "https://kubecost-eks-cost-analyzer.kubecost-eks"
   kubecost_allocation_api_read_timeout = 30
   kubecost_assets_api_read_timeout     = 10
+  tls_verify                           = "no"
 }
 
 module "cluster2-us-east-1-111111111111" {
