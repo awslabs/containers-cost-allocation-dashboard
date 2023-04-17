5ca61d9c039abc1c0cdfa6695eca637557c048bc
Udi Dahan <udid@amazon.com>
2023-04-17 07:01:31 +0000

    - Added example for cluster without Helm invocation

--- terraform/kubecost_cid_terraform_module/deploy/main.tf
+++ terraform/kubecost_cid_terraform_module/deploy/main.tf
@@ -8,6 +8,8 @@ module "pipeline" {
 }
 
 # Module instances for the kubecost_s3_exporter module, to create IRSA and deploy the Kubecost S3 Exporter pod
+
+# Example module instance for cluster with Helm invocation
 module "cluster1" {
 
   # This is an example, to help you get started
@@ -18,6 +20,23 @@ module "cluster1" {
     aws.pipeline = aws
     aws.eks      = aws.us-east-1-111111111111-cluster1
     helm         = helm.us-east-1-111111111111-cluster1
+  }
+
+  cluster_arn                          = ""
+  cluster_oidc_provider_arn            = ""
+  kubecost_s3_exporter_container_image = ""
+}
+
+# Example module instance for cluster without Helm invocation
+module "cluster2" {
+
+  # This is an example, to help you get started
+
+  source = "../modules/kubecost_s3_exporter"
+
+  providers = {
+    aws.pipeline = aws
+    aws.eks      = aws.us-east-1-111111111111-cluster2
   }
 
   cluster_arn                          = ""
