43829268d5c07722c47c258783fdbb4c7251b70b
Udi Dahan <udid@amazon.com>
2023-04-17 08:21:36 +0000

    - Changed the required modules version constraint to be "<=", to not force upgrades in customers' environments

--- terraform/kubecost_cid_terraform_module/modules/pipeline/pipeline.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/pipeline.tf
@@ -8,11 +8,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.63.0"
+      version = "<= 4.63.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "= 2.4.0"
+      version = "<= 2.4.0"
     }
   }
 }
