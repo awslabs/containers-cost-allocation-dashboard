5ec45662cd594e3452c6056a33dbececa7967075
Udi Dahan <udid@amazon.com>
2023-05-16 10:21:02 +0000

    - Added logic to not create the labels output in case no labels were given for any cluster

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -1,7 +1,7 @@
 # Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "labels" {
-  value       = local.labels_for_output
+  value       = length(local.labels_for_output) > 0 ? local.labels_for_output : null
   description = "List of the distinct labels from all clusters"
 }
 
