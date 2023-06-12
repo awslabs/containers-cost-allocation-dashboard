455f7d3b4c008988e124f6a871eb526086f8e091
Udi Dahan <udid@amazon.com>
2023-06-12 08:52:43 +0000

    - Updated the copyright header

--- terraform/kubecost_cid_terraform_module/deploy/outputs.tf
+++ terraform/kubecost_cid_terraform_module/deploy/outputs.tf
@@ -1,4 +1,4 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+# Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 # Output for showing the distinct labels from all clusters, collected from the "clusters_labels" common input
 output "labels" {
