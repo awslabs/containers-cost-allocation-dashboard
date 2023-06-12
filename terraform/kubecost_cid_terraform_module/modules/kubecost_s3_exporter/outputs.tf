e5cc733d35617820e14405002e6345609d75bff4
Udi Dahan <udid@amazon.com>
2023-06-12 08:48:13 +0000

    - Updated the copyright header

--- terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
@@ -1,4 +1,4 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+# Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "irsa_child_iam_role_arn" {
   value       = aws_iam_role.kubecost_s3_exporter_irsa_child_role.arn
