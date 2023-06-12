973da26fd1eaf1b2689a3867231cfe5b78cbdf80
Udi Dahan <udid@amazon.com>
2023-06-12 08:46:44 +0000

    - Updated the copyright header

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -1,4 +1,4 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+# Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "labels" {
   value       = length(local.labels_for_output) > 0 ? local.labels_for_output : null
