4a0444a3ea6103f816ae0cc14a14748ca66fd069
Udi Dahan <udid@amazon.com>
2023-06-12 08:50:04 +0000

    - Updated the copyright header

--- terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
+++ terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
@@ -1,4 +1,4 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+# Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "bucket_arn" {
   type        = string
