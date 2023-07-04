440798627797d60e7d2d4c28611277d1c4dfd3d5
Udi Dahan <udid@amazon.com>
2023-07-04 15:44:50 +0000

    - Added support for K8s annotations

--- terraform/kubecost_cid_terraform_module/examples/deploy/outputs.tf
+++ terraform/kubecost_cid_terraform_module/examples/deploy/outputs.tf
@@ -1,9 +1,15 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
-# Output for showing the distinct labels from all clusters, collected from the "clusters_labels" common input
+# Output for showing the distinct labels from all clusters, collected from the "clusters_metadata" common input
 output "labels" {
   value       = module.pipeline.labels
-  description = "A list of the distinct lab of all clusters, that'll be added to the dataset"
+  description = "A list of the distinct labels of all clusters, that'll be added to the dataset"
+}
+
+# Output for showing the distinct annotations from all clusters, collected from the "clusters_metadata" common input
+output "annotations" {
+  value       = module.pipeline.annotations
+  description = "A list of the distinct annotations of all clusters, that'll be added to the dataset"
 }
 
 # Clusters outputs
