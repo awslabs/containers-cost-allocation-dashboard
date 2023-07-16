7359c9645e73eaf9d4d1ac57c27795090c259a6d
Udi Dahan <udid@amazon.com>
2023-07-16 14:36:46 +0000

    - Added logic to create a custom Athena Workgroup

--- terraform/kubecost_cid_terraform_module/modules/pipeline/pipeline.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/pipeline.tf
@@ -26,6 +26,31 @@ locals {
   annotations_for_output = join(", ", local.distinct_annotations)
 }
 
+# This data source is used conditionally, only if the "create" field in the "custom_athena_workgroup" variable is "true"
+data aws_kms_key "s3_kms" {
+  count = var.custom_athena_workgroup.create ? 1 : 0
+
+  key_id = "alias/aws/s3"
+}
+
+# This resource is created conditionally, only if the "create" field in the "custom_athena_workgroup" variable is "true"
+resource "aws_athena_workgroup" "kubecost_athena_workgroup" {
+  count = var.custom_athena_workgroup.create ? 1 : 0
+
+  name = "kubecost"
+  force_destroy = true
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${var.custom_athena_workgroup.query_results_location_bucket_name}/"
+        encryption_configuration {
+          encryption_option = "SSE_KMS"
+          kms_key_arn = data.aws_kms_key.s3_kms[count.index].arn
+      }
+    }
+  }
+}
+
 resource "aws_iam_policy" "kubecost_glue_crawler_policy" {
   name = "kubecost_glue_crawler_policy"
   policy = jsonencode(
