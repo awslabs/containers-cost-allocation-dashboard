7188c4007be277cd73ee6e1b5d7a3e03cb904927
Udi Dahan <udid@amazon.com>
2023-07-17 10:23:25 +0000

    - Removed custom Athena Workgroup variable (moved to the common module)

--- terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
@@ -9,17 +9,3 @@ variable "glue_crawler_schedule" {
     error_message = "The 'glue_crawler_schedule' input is empty. It must contain a Cron expression"
   }
 }
-
-variable "custom_athena_workgroup" {
-  description = "The settings for the custom Athena Workgroup. This variable can either have 'create' field as 'true' with 'query_results_location_bucket_name' containing a valid S3 bucket name, or 'create' field as 'false'"
-
-  type = object({
-    create                             = optional(bool, true) # Dictates whether to create a custom Athena Workgroup
-    query_results_location_bucket_name = optional(string, "") # If "create" is "true", used to define the Athena Workgroup query results location S3 bucket name
-  })
-
-  validation {
-    condition     = (var.custom_athena_workgroup.create && can(regex("^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.custom_athena_workgroup.query_results_location_bucket_name)) && !startswith(var.custom_athena_workgroup.query_results_location_bucket_name, "xn--") && !endswith(var.custom_athena_workgroup.query_results_location_bucket_name, "-s3alias")) || (!var.custom_athena_workgroup.create)
-    error_message = "The 'custom_athena_workgroup' variable must either have 'create' field as 'true' and a valid S3 bucket name in 'query_results_location_bucket_name' field, or 'create' field as 'false'"
-  }
-}
