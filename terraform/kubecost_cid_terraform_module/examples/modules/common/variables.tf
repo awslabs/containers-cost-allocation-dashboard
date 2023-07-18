9f331a228df59d5453dc99b4679ea66ff3cab317
Udi Dahan <udid@amazon.com>
2023-07-18 08:27:41 +0000

    - Styling changes - Changed the "name" field in the Athena Workgroup variable to be required, and updated the input validation

--- terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
+++ terraform/kubecost_cid_terraform_module/examples/modules/common/variables.tf
@@ -1,34 +1,39 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "bucket_arn" {
+  description = "(Required) The ARN of the S3 Bucket to which the Kubecost data will be uploaded"
   type        = string
   default     = "arn:aws:s3:::kubecost-data-collection-bucket"
-  description = "The ARN of the S3 Bucket to which the Kubecost data will be uploaded"
 
-  # The below validation validates the "bucket_arn" input.
-  # It'll return the specified error if the input fails the validation.
-  # Here are a few examples of a "bucket_arn" input that'll fail validation:
-  # The "bucket_arn" key is empty ("")
-  # The "bucket_arn" key has some unstructured string (e.g. "test")
-  # The "bucket_arn" key has less than 6 ARN fields ("arn:aws:eks")
-  # The bucket name part of the ARN doesn't match the bucket name rules
-  # The "bucket_arn" input has missing ARN fields, has value in fields where there shouldn't be, or has incorrect value in some ARN fields. A few examples:
-  # arn:aws:s3:us-east-1:111111111111:bucket_name
-  # arn:aws:eks:::bucket_name
-  # arn:aaa:s3:::bucket_name
-  #
   # Note - the full regex should have been "^arn:(?:aws|aws-cn|aws-us-gov):s3:::(?!(xn--|.+-s3alias$))[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$"
   # The "(?!(xn--|.+-s3alias$))" part has been omitted because Terraform regex engine doesn't support negative lookahead (the "?!" part)
   # Therefore, it has been removed, and instead, "!startswith" and "!endswith" conditions have been added, to complement this missing functionality
   validation {
-    condition     = can(regex("^arn:(?:aws|aws-cn|aws-us-gov):s3:::[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.bucket_arn)) && !startswith(element(split(":::", var.bucket_arn), 1), "xn--") && !endswith(element(split(":::", var.bucket_arn), 1), "-s3alias")
-    error_message = "The 'bucket_arn' input contains an invalid ARN"
+    condition = (
+      can(regex("^arn:(?:aws|aws-cn|aws-us-gov):s3:::[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.bucket_arn)) &&
+      !startswith(element(split(":::", var.bucket_arn), 1), "xn--") &&
+      !endswith(element(split(":::", var.bucket_arn), 1), "-s3alias")
+    )
+    error_message = "The 'bucket_arn' variable contains an invalid ARN"
   }
 }
 
 variable "clusters_metadata" {
+  description = <<EOF
+    (Optional) A list of clusters and their additional metadata (K8s labels, annotations) that you wish to include in the dataset.
+    Each item in the list has the following parameters:
+
+    (Required) cluster_id: The unique ID of the K8s cluster (e.g., ARN for EKS cluster)
+    (Optional) labels: A list of K8s labels you wish to include in the dataset
+    (Optional) annotations: A list of K8s annotations you wish to include in the dataset
+
+    Please note that there's no need to include a "cluster_id" field in the list if you don't need to add labels or annotations to the dataset for it.
+    In other words, when adding a cluster to the list, add it with at least "labels" field or "annotations" field.
+  EOF
+
   type = list(object({
     cluster_id  = string
+    labels      = optional(list(string))
     annotations = optional(list(string))
   }))
 
@@ -60,37 +65,30 @@ variable "clusters_metadata" {
     }
   ]
 
-  description = "A map of clusters and their additional metadata (K8s labels, annotations) that you wish to include in the dataset"
-
-  # The below validation validates each "cluster_arn" key's value in each object in the "clusters_labels" list.
-  # It'll return the specified error if at least one of the "cluster_arn" keys' value fails the validation.
-  # Here are a few examples of a "cluster_arn" key's value that'll fail validation:
-  # The "cluster_arn" key's value is empty ("")
-  # The "cluster_arn" key's value has some unstructured string (e.g. "test")
-  # The "cluster_arn" key's value has less than 6 ARN fields ("arn:aws:eks")
-  # The "cluster_arn" key's value has 6 ARN fields, but is missing the "/" before the resource ID ("arn:aws:eks:us-east-1:111111111111:cluster")
-  # The EKS cluster name part of the ARN doesn't match the EKS cluster name rules
-  # The "cluster_arn" key's value has missing ARN fields or has incorrect value in some ARN fields. A few examples:
-  # arn:aws:eks:us-east-1:111111111111:cluster/
-  # arn:aws:eks:us-east-1:123:cluster/cluster1
-  # arn:aws:eks:us-east-1::cluster/cluster1
-  # arn:aws:eks:aaa:111111111111:cluster/cluster1
-  # arn:aws:eks::111111111111:cluster/cluster1
-  # arn:aws:s3:us-east-1:111111111111:cluster/cluster1
-  # arn:aaa:eks:us-east-1:111111111111:cluster/cluster1
   validation {
-    condition     = length([for cluster_id in var.clusters_metadata.*.cluster_id : cluster_id if can(regex("^arn:(?:aws|aws-cn|aws-us-gov):eks:(?:us(?:-gov)?|ap|ca|cn|eu|sa)-(?:central|(?:north|south)?(?:east|west)?)-\\d:\\d{12}:cluster\\/[a-zA-Z0-9][a-zA-Z0-9-_]{1,99}$", cluster_id))]) == length(var.clusters_metadata)
+    condition = (
+      length([
+        for cluster_id in var.clusters_metadata.*.cluster_id : cluster_id
+        if can(regex("^arn:(?:aws|aws-cn|aws-us-gov):eks:(?:us(?:-gov)?|ap|ca|cn|eu|sa)-(?:central|(?:north|south)?(?:east|west)?)-\\d:\\d{12}:cluster\\/[a-zA-Z0-9][a-zA-Z0-9-_]{1,99}$", cluster_id))
+      ]) == length(var.clusters_metadata)
+    )
     error_message = "At least one of the 'cluster_id' keys in the 'clusters_metadata' list, contains an invalid value"
   }
 }
 
-variable "custom_athena_workgroup" {
-  description = "The settings for the custom Athena Workgroup. This variable can either have 'create' field as 'true' with 'name' field containing a valid Athena Workgroup name and 'query_results_location_bucket_name' containing a valid S3 bucket name, or 'create' field as 'false'"
+variable "athena_workgroup_configuration" {
+  description = <<EOF
+    (Optional) The configuration the Athena Workgroup (used either to create a new Athena Workgroup, or reference configuration of an existing Athena Workgroup.
+
+    (Required) create: Dictates whether to create a custom Athena Workgroup
+    (Required) name: If "create" is "true", used to define the Athena Workgroup name and reference it in the QuickSight Data Source. If "create" is "false", used only for referencing the Workgroup in the QuickSight Data Source
+    (Required when "create" is "true') query_results_location_bucket_name: If "create" is "true", used to set the Athena Workgroup query results location. If "create" is "false", this field is ignored
+  EOF
 
   type = object({
-    create                             = bool                 # Dictates whether to create a custom Athena Workgroup
-    name                               = optional(string, "") # If "create" is "true", used to define the Athena Workgroup name
-    query_results_location_bucket_name = optional(string, "") # If "create" is "true", used to define the Athena Workgroup query results location S3 bucket name
+    create                             = bool
+    name                               = string
+    query_results_location_bucket_name = optional(string, "")
   })
 
   default = {
@@ -100,12 +98,37 @@ variable "custom_athena_workgroup" {
   }
 
   validation {
-    condition     = (var.custom_athena_workgroup.create && can(regex("^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.custom_athena_workgroup.query_results_location_bucket_name)) && !startswith(var.custom_athena_workgroup.query_results_location_bucket_name, "xn--") && !endswith(var.custom_athena_workgroup.query_results_location_bucket_name, "-s3alias")) && can(regex("^[A-Za-z0-9_.-]{1,128}$", var.custom_athena_workgroup.name)) || (!var.custom_athena_workgroup.create)
-    error_message = "The 'custom_athena_workgroup' variable must have one of the following combinations:\n 1. When the 'create' field is 'true', the 'name' field must have a valid Athena Workgroup name, and the 'query_results_location_bucket_name' field must have a valid S3 bucket name\n 2. When the 'create' field is 'false', the 'name' and 'query_results_location_bucket_name' can have any value, as they're ignored"
+    condition = (
+      (
+        var.athena_workgroup_configuration.create &&
+        can(regex("^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.athena_workgroup_configuration.query_results_location_bucket_name)) &&
+        !startswith(var.athena_workgroup_configuration.query_results_location_bucket_name, "xn--") &&
+        !endswith(var.athena_workgroup_configuration.query_results_location_bucket_name, "-s3alias") &&
+        can(regex("^[A-Za-z0-9_.-]{1,128}$", var.athena_workgroup_configuration.name))
+      )
+      ||
+      (
+        !var.athena_workgroup_configuration.create &&
+        can(regex("^[A-Za-z0-9_.-]{1,128}$", var.athena_workgroup_configuration.name))
+      )
+    )
+    error_message = <<EOF
+      The 'athena_workgroup_configuration' variable must have one of the following combinations:
+1. When the 'create' field is 'true', the 'name' field must have a valid Athena Workgroup name, and the 'query_results_location_bucket_name' field must have a valid S3 bucket name
+2. When the 'create' field is 'false', the 'name' field must have a valid Athena Workgroup name, and 'query_results_location_bucket_name' is ignored (so it can have any value)
+    EOF
   }
 }
 
 variable "kubecost_ca_certificates_list" {
+  description = <<EOF
+    (Optional) A list root CA certificates paths and their configuration for AWS Secrets Manager. Used for TLS communication with Kubecost. This is a consolidated list of all root CA certificates that are needed for all Kubecost endpoints.
+
+    (Required) cert_path: The full local path to the root CA certificate
+    (Required) cert_secret_name: The name to use for the AWS Secrets Manager Secret that will be created for this root CA certificate
+    (Optional) cert_secret_allowed_principals: A list of principals to include in the AWS Secrets Manager Secret policy (in addition to the principal that identify the cluster, which will be automatically added by Terraform)
+  EOF
+
   type = list(object({
     cert_path                      = string
     cert_secret_name               = string
@@ -123,18 +146,22 @@ variable "kubecost_ca_certificates_list" {
       "cert_secret_name" : "cert_secret2"
     }
   ]
-  description = "A list of objects containing CA certificates paths and their desired secret name in AWS Secrets Manager"
 
   validation {
-    condition     = length([for cert_secret_name in var.kubecost_ca_certificates_list.*.cert_secret_name : cert_secret_name if can(regex("^[a-z[A-Z0-9/_+=.@-]{1,512}$", cert_secret_name))]) == length(var.kubecost_ca_certificates_list)
+    condition = (
+      length([
+        for cert_secret_name in var.kubecost_ca_certificates_list.*.cert_secret_name : cert_secret_name
+        if can(regex("^[a-z[A-Z0-9/_+=.@-]{1,512}$", cert_secret_name))
+      ]) == length(var.kubecost_ca_certificates_list)
+    )
     error_message = "At least one of the 'cert_secret_name' keys in the 'kubecost_ca_certificates_list' list, contains an invalid secret name"
   }
 }
 
 variable "aws_shared_config_files" {
+  description = "(Optional) Paths to the AWS shared config files"
   type        = list(string)
   default     = ["~/.aws/config"]
-  description = "Paths to the AWS shared config files"
 
   # Here are a few examples of a "aws_shared_config_files" input that'll fail validation:
   # The "aws_shared_config_files" has an empty list: []
@@ -146,9 +173,9 @@ variable "aws_shared_config_files" {
 }
 
 variable "aws_shared_credentials_files" {
+  description = "(Optional) Paths to the AWS shared credentials files"
   type        = list(string)
   default     = ["~/.aws/credentials"]
-  description = "Paths to the AWS shared credentials files"
 
   # Here are a few examples of a "aws_shared_credentials_files" input that'll fail validation:
   # The "aws_shared_credentials_files" has an empty list: []
@@ -160,10 +187,10 @@ variable "aws_shared_credentials_files" {
 }
 
 variable "aws_common_tags" {
-  type = map(any)
+  description = "(Optional) Common AWS tags to be used on all AWS resources created by Terraform"
+  type        = map(any)
   default = {
     test-tag1 = "test-value1"
     test-tag2 = "test-value2"
   }
-  description = "Common AWS tags to be used on all AWS resources created by Terraform"
 }
