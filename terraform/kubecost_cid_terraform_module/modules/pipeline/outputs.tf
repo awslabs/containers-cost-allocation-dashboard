a946daec4d6819ecf4b49746f0ed869a6c815fc4
Udi Dahan <udid@amazon.com>
2023-07-18 10:23:19 +0000

    - Styling changes

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -1,21 +1,21 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "labels" {
-  value       = length(local.labels_for_output) > 0 ? local.labels_for_output : null
   description = "List of the distinct labels from all clusters"
+  value       = length(local.labels_for_output) > 0 ? local.labels_for_output : null
 }
 
 output "annotations" {
-  value       = length(local.annotations_for_output) > 0 ? local.annotations_for_output : null
   description = "List of the distinct annotations from all clusters"
+  value       = length(local.annotations_for_output) > 0 ? local.annotations_for_output : null
 }
 
 output "kubecost_ca_cert_secret" {
-  value       = aws_secretsmanager_secret.kubecost_ca_cert_secret
   description = "All AWS Secrets Manager Secrets"
+  value       = aws_secretsmanager_secret.kubecost_ca_cert_secret
 }
 
 output "custom_athena_workgroup" {
-  value       = aws_athena_workgroup.kubecost_athena_workgroup
   description = "The configuration the Athena Workgroup. Used either to create a new Athena Workgroup, or reference configuration of an existing Athena Workgroup"
+  value       = aws_athena_workgroup.kubecost_athena_workgroup
 }
