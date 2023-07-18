6a3852266a19c202e17c5021f78ad1a5d2fb5053
Udi Dahan <udid@amazon.com>
2023-07-18 10:21:28 +0000

    - Styling changes

--- terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/outputs.tf
@@ -1,11 +1,11 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 output "irsa_child_iam_role_arn" {
-  value       = aws_iam_role.kubecost_s3_exporter_irsa_child_role.arn
   description = "The ARN of the child IAM Role that was created as part of IRSA role chaining"
+  value       = aws_iam_role.kubecost_s3_exporter_irsa_child_role.arn
 }
 
 output "irsa_parent_iam_role_arn" {
-  value       = aws_iam_role.kubecost_s3_exporter_irsa_parent_role.arn
   description = "The ARN of the parent IAM Role that was created as part of IRSA role chaining"
+  value       = aws_iam_role.kubecost_s3_exporter_irsa_parent_role.arn
 }
