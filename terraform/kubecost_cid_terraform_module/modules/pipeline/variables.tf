7a4ce79c79c4821d095b8ddc57e28549bd64e82f
Udi Dahan <udid@amazon.com>
2023-07-18 09:38:32 +0000

    - Styling changes

--- terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/variables.tf
@@ -1,11 +1,11 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "glue_crawler_schedule" {
+  description = "(Required) The schedule for the Glue Crawler, in Cron format. Make sure to set it after the last Kubecost S3 Exporter Cron schedule"
   type        = string
-  description = "The schedule for the Glue Crawler, in Cron format. Make sure to set it after the last Kubecost S3 Exporter Cron schedule"
 
   validation {
     condition     = var.glue_crawler_schedule != ""
-    error_message = "The 'glue_crawler_schedule' input is empty. It must contain a Cron expression"
+    error_message = "The 'glue_crawler_schedule' variable is empty. It must contain a Cron expression"
   }
 }
