416b210713bcc31142013cb8ed417892db487c86
Udi Dahan <udid@amazon.com>
2023-07-31 07:07:38 +0000

    Update 6 files
    - /terraform/terraform_aws_helm_resources/locals.tf
    - /terraform/terraform_aws_helm_resources/main.tf
    - /terraform/terraform_aws_helm_resources/providers.tf
    - /terraform/terraform_aws_resources/locals.tf
    - /terraform/terraform_aws_resources/main.tf
    - /terraform/terraform_aws_resources/providers.tf

--- terraform/terraform_aws_resources/providers.tf
+++
@@ -1,5 +0,0 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
-
-provider "aws" {
-  region = local.region
-}
