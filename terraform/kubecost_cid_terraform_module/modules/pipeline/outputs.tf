94c5b4f0b86ff2f9fbde722a29870c5164254bc6
Udi Dahan <udid@amazon.com>
2023-09-12 06:30:20 +0000

    - Removed Athena workgroup output, in intention to move it to the future dashboard module - Removed Glue output, as it's not used anymore

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -4,13 +4,3 @@ output "kubecost_ca_cert_secret" {
   description = "All AWS Secrets Manager Secrets"
   value       = aws_secretsmanager_secret.kubecost_ca_cert_secret
 }
-
-output "custom_athena_workgroup" {
-  description = "The configuration the Athena Workgroup. Used either to create a new Athena Workgroup, or reference configuration of an existing Athena Workgroup"
-  value       = aws_athena_workgroup.kubecost_athena_workgroup
-}
-
-output "aws_glue_table" {
-  description = "The AWS Glue Table used to store the Kubecost data"
-  value       = aws_glue_catalog_table.kubecost_glue_table
-}
