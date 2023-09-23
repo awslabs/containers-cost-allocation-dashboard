e9a0cc77fc1c476b95b6fad888f7ea8d32fd6049
Udi Dahan <udid@amazon.com>
2023-09-23 07:10:07 +0000

    - Removed locals in favor of separate locals.tf file - Added EKS cluster data source and IAM oidc provider data source   - This removes the need for the IAM OIDC Provider ARN variable   - Replaced all references of the above variable, with the data source ARN attribute

--- terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/kubecost_s3_exporter.tf
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/kubecost_s3_exporter.tf
@@ -30,94 +30,16 @@ data "aws_caller_identity" "eks_caller_identity" {
   provider = aws.eks
 }
 
-locals {
-  cluster_region                     = element(split(":", var.cluster_arn), 3)
-  cluster_account_id                 = element(split(":", var.cluster_arn), 4)
-  cluster_name                       = element(split("/", var.cluster_arn), 1)
-  cluster_oidc_provider_id           = element(split("/", var.cluster_oidc_provider_arn), 3)
-  pipeline_partition                 = element(split(":", data.aws_caller_identity.pipeline_caller_identity.arn), 1)
-  pipeline_account_id                = data.aws_caller_identity.pipeline_caller_identity.account_id
-  kubecost_ca_certificate_secret_arn = length(var.kubecost_ca_certificate_secrets) > 0 ? lookup(element(var.kubecost_ca_certificate_secrets, index(var.kubecost_ca_certificate_secrets.*.name, var.kubecost_ca_certificate_secret_name)), "arn", "") : ""
-  helm_chart_location                = "../../../helm/kubecost_s3_exporter"
-  helm_values_yaml = yamlencode(
-    {
-      "namespace" : var.namespace
-      "image" : var.kubecost_s3_exporter_container_image
-      "imagePullPolicy" : var.kubecost_s3_exporter_container_image_pull_policy
-      "ephemeralVolumeSize" : var.kubecost_s3_exporter_ephemeral_volume_size
-      "cronJob" : {
-        "name" : "kubecost-s3-exporter",
-        "schedule" : var.kubecost_s3_exporter_cronjob_schedule
-      }
-      "serviceAccount" : {
-        "name" : var.service_account
-        "create" : var.create_service_account
-        "role" : length(aws_iam_role.kubecost_s3_exporter_irsa_child_role) > 0 ? aws_iam_role.kubecost_s3_exporter_irsa_child_role[0].arn : aws_iam_role.kubecost_s3_exporter_irsa_role[0].arn
-      }
-      "env" : [
-        {
-          "name" : "S3_BUCKET_NAME",
-          "value" : element(split(":::", module.common.bucket_arn), 1)
-        },
-        {
-          "name" : "KUBECOST_API_ENDPOINT",
-          "value" : var.kubecost_api_endpoint
-        },
-        {
-          "name" : "BACKFILL_PERIOD_DAYS",
-          "value" : var.backfill_period_days
-        },
-        {
-          "name" : "CLUSTER_ID",
-          "value" : var.cluster_arn
-        },
-        {
-          "name" : "IRSA_PARENT_IAM_ROLE_ARN",
-          "value" : length(aws_iam_role.kubecost_s3_exporter_irsa_parent_role) > 0 ? aws_iam_role.kubecost_s3_exporter_irsa_parent_role[0].arn : ""
-        },
-        {
-          "name" : "AGGREGATION",
-          "value" : var.aggregation
-        },
-        {
-          "name" : "KUBECOST_ALLOCATION_API_PAGINATE",
-          "value" : var.kubecost_allocation_api_paginate
-        },
-        {
-          "name" : "CONNECTION_TIMEOUT",
-          "value" : var.connection_timeout
-        },
-        {
-          "name" : "KUBECOST_ALLOCATION_API_READ_TIMEOUT",
-          "value" : var.kubecost_allocation_api_read_timeout
-        },
-        {
-          "name" : "TLS_VERIFY",
-          "value" : var.tls_verify
-        },
-        {
-          "name" : "KUBECOST_CA_CERTIFICATE_SECRET_NAME",
-          "value" : length(local.kubecost_ca_certificate_secret_arn) > 0 ? var.kubecost_ca_certificate_secret_name : ""
-        },
-        {
-          "name" : "KUBECOST_CA_CERTIFICATE_SECRET_REGION",
-          "value" : length(local.kubecost_ca_certificate_secret_arn) > 0 ? element(split(":", local.kubecost_ca_certificate_secret_arn), 3) : ""
-        },
-        {
-          "name" : "LABELS",
-          "value" : join(", ", distinct(module.common.k8s_labels))
-        },
-        {
-          "name" : "ANNOTATIONS",
-          "value" : join(", ", distinct(module.common.k8s_annotations))
-        },
-        {
-          "name" : "PYTHONUNBUFFERED",
-          "value" : "1"
-        }
-      ]
-    }
-  )
+data "aws_eks_cluster" "cluster" {
+  provider = aws.eks
+
+  name = local.cluster_name
+}
+
+data "aws_iam_openid_connect_provider" "oidc" {
+  provider = aws.eks
+
+  url = data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer
 }
 
 # The below resource is created conditionally, if the EKS cluster account ID and the pipeline account ID are the same
@@ -127,7 +49,7 @@ resource "aws_iam_role" "kubecost_s3_exporter_irsa_role" {
 
   count = data.aws_caller_identity.eks_caller_identity.account_id == data.aws_caller_identity.pipeline_caller_identity.account_id ? 1 : 0
 
-  name = "kubecost_s3_exporter_irsa_${element(split("/", var.cluster_oidc_provider_arn), 3)}"
+  name = "kubecost_s3_exporter_irsa_${local.cluster_oidc_provider_id}"
   assume_role_policy = jsonencode(
     {
       Version = "2012-10-17"
@@ -135,13 +57,13 @@ resource "aws_iam_role" "kubecost_s3_exporter_irsa_role" {
         {
           Effect = "Allow"
           Principal = {
-            Federated = var.cluster_oidc_provider_arn
+            Federated = data.aws_iam_openid_connect_provider.oidc.arn
           }
           Action = "sts:AssumeRoleWithWebIdentity"
           Condition = {
             StringEquals = {
-              "${element(split(":oidc-provider/", var.cluster_oidc_provider_arn), 1)}:aud" = "sts.amazonaws.com"
-              "${element(split(":oidc-provider/", var.cluster_oidc_provider_arn), 1)}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account}"
+              "${element(split(":oidc-provider/", data.aws_iam_openid_connect_provider.oidc.arn), 1)}:aud" = "sts.amazonaws.com"
+              "${element(split(":oidc-provider/", data.aws_iam_openid_connect_provider.oidc.arn), 1)}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account}"
             }
           }
         }
@@ -217,7 +139,7 @@ resource "aws_iam_role" "kubecost_s3_exporter_irsa_child_role" {
 
   count = data.aws_caller_identity.eks_caller_identity.account_id != data.aws_caller_identity.pipeline_caller_identity.account_id ? 1 : 0
 
-  name = "kubecost_s3_exporter_irsa_${element(split("/", var.cluster_oidc_provider_arn), 3)}"
+  name = "kubecost_s3_exporter_irsa_${local.cluster_oidc_provider_id}"
   assume_role_policy = jsonencode(
     {
       Version = "2012-10-17"
@@ -225,13 +147,13 @@ resource "aws_iam_role" "kubecost_s3_exporter_irsa_child_role" {
         {
           Effect = "Allow"
           Principal = {
-            Federated = var.cluster_oidc_provider_arn
+            Federated = data.aws_iam_openid_connect_provider.oidc.arn
           }
           Action = "sts:AssumeRoleWithWebIdentity"
           Condition = {
             StringEquals = {
-              "${element(split(":oidc-provider/", var.cluster_oidc_provider_arn), 1)}:aud" = "sts.amazonaws.com"
-              "${element(split(":oidc-provider/", var.cluster_oidc_provider_arn), 1)}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account}"
+              "${element(split(":oidc-provider/", data.aws_iam_openid_connect_provider.oidc.arn), 1)}:aud" = "sts.amazonaws.com"
+              "${element(split(":oidc-provider/", data.aws_iam_openid_connect_provider.oidc.arn), 1)}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account}"
             }
           }
         }
@@ -240,7 +162,7 @@ resource "aws_iam_role" "kubecost_s3_exporter_irsa_child_role" {
   )
 
   inline_policy {
-    name = "kubecost_s3_exporter_irsa_${element(split("/", var.cluster_oidc_provider_arn), 3)}"
+    name = "kubecost_s3_exporter_irsa_${local.cluster_oidc_provider_id}"
     policy = jsonencode(
       {
         Statement = [
