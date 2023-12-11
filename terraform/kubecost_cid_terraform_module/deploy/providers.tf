0a99cc0b5625e0e2a0afbe1a1b5b02e39046d2de
Udi Dahan <udid@amazon.com>
2023-12-11 07:09:22 +0000

    - Improved comments - Added quicksight module provider

--- terraform/kubecost_cid_terraform_module/deploy/providers.tf
+++ terraform/kubecost_cid_terraform_module/deploy/providers.tf
@@ -4,21 +4,32 @@ module "common" {
   source = "../modules/common"
 }
 
+######################################
+# Section 1 - Pipeline AWS Provider  #
+######################################
+
 # Example provider for the pipeline
 provider "aws" {
 
   # This is an example, to help you get started
 
   region                   = "us-east-1"
-  shared_config_files      = module.common.aws_shared_config_files
-  shared_credentials_files = module.common.aws_shared_credentials_files
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "pipeline_profile"
   default_tags {
     tags = module.common.aws_common_tags
   }
 }
 
-# Example providers for cluster with Helm invocation
+###########################################################
+# Section 2 - Kubecost S3 Exporter AWS and Helm Providers #
+###########################################################
+
+#                                                    #
+# Example providers for cluster with Helm invocation #
+#                                                    #
+
 provider "aws" {
 
   # This is an example, to help you get started
@@ -26,8 +37,8 @@ provider "aws" {
   alias = "us-east-1-111111111111-cluster1"
 
   region                   = "us-east-1"
-  shared_config_files      = module.common.aws_shared_config_files
-  shared_credentials_files = module.common.aws_shared_credentials_files
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "profile1"
   default_tags {
     tags = module.common.aws_common_tags
@@ -46,7 +57,10 @@ provider "helm" {
   }
 }
 
-# Example providers for cluster without Helm invocation
+#                                                        #
+# Example providers for cluster without Helm invocation  #
+#                                                        #
+
 provider "aws" {
 
   # This is an example, to help you get started
@@ -54,9 +68,28 @@ provider "aws" {
   alias = "us-east-1-111111111111-cluster2"
 
   region                   = "us-east-1"
-  shared_config_files      = module.common.aws_shared_config_files
-  shared_credentials_files = module.common.aws_shared_credentials_files
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "profile1"
+  default_tags {
+    tags = module.common.aws_common_tags
+  }
+}
+
+#######################################
+# Section 3 - Quicksight AWS Provider #
+#######################################
+
+provider "aws" {
+
+  # This is an example, to help you get started
+
+  alias = "quicksight"
+
+  region                   = "us-east-1"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "quicksight_profile"
   default_tags {
     tags = module.common.aws_common_tags
   }
