06b2330cae542485d246688d8c0ef52b3e638b08
Udi Dahan <udid@amazon.com>
2023-12-11 07:00:23 +0000

    - Improved variables' descriptions

--- terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/variables.tf
+++ terraform/kubecost_cid_terraform_module/modules/kubecost_s3_exporter/variables.tf
@@ -1,8 +1,12 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
 variable "cluster_arn" {
-  description = "(Required) The EKS cluster ARN in which the Kubecost S3 Exporter pod will be deployed"
-  type        = string
+  description = <<-EOF
+    (Required) The EKS cluster ARN in which the Kubecost S3 Exporter pod will be deployed
+               Possible values: EKS cluster ARN.
+  EOF
+
+  type = string
 
   validation {
     condition     = can(regex("^arn:(?:aws|aws-cn|aws-us-gov):eks:(?:us(?:-gov)?|ap|ca|cn|eu|sa)-(?:central|(?:north|south)?(?:east|west)?)-\\d:\\d{12}:cluster\\/[a-zA-Z0-9][\\w-]{1,99}$", var.cluster_arn))
@@ -11,8 +15,12 @@ variable "cluster_arn" {
 }
 
 variable "kubecost_s3_exporter_container_image" {
-  description = "(Required) The Kubecost S3 Exporter container image"
-  type        = string
+  description = <<-EOF
+    (Required) The Kubecost S3 Exporter container image.
+               Possible values: A valid Docker image string.
+  EOF
+
+  type = string
 
   validation {
     condition     = can(regex("^((?:((?:(?:localhost|[\\w-]+(?:\\.[\\w-]+)+)(?::\\d+)?)|[\\w]+:\\d+)\\/)?\\/?((?:(?:[a-z0-9]+(?:(?:[._]|__|[-]*)[a-z0-9]+)*)\\/)*)([a-z0-9_-]+))[:@]?(([\\w][\\w.-]{0,127})|([A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][0-9A-Fa-f]{32,}))?$", var.kubecost_s3_exporter_container_image))
@@ -21,9 +29,14 @@ variable "kubecost_s3_exporter_container_image" {
 }
 
 variable "kubecost_s3_exporter_container_image_pull_policy" {
-  description = "(Optional) The image pull policy that'll be used by the Kubecost S3 Exporter pod"
-  type        = string
-  default     = "Always"
+  description = <<-EOF
+    (Optional) The image pull policy that will be used by the Kubecost S3 Exporter pod.
+               Possible values: "Always", "IfNotPresent" or "Never".
+               Default value: Always
+  EOF
+
+  type    = string
+  default = "Always"
 
   validation {
     condition     = contains(["Always", "IfNotPresent", "Never"], var.kubecost_s3_exporter_container_image_pull_policy)
@@ -32,9 +45,14 @@ variable "kubecost_s3_exporter_container_image_pull_policy" {
 }
 
 variable "kubecost_s3_exporter_cronjob_schedule" {
-  description = "(Optional) The schedule of the Kubecost S3 Exporter CronJob"
-  type        = string
-  default     = "0 0 * * *"
+  description = <<-EOF
+    (Optional) The schedule of the Kubecost S3 Exporter CronJob.
+               Possible values: A valid cron expression.
+               Default value: 0 0 * * *
+  EOF
+
+  type    = string
+  default = "0 0 * * *"
 
   validation {
     condition     = can(regex("(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+([/\\-])\\d+)|\\d+|\\*) ?){5,7})", var.kubecost_s3_exporter_cronjob_schedule))
@@ -43,9 +61,14 @@ variable "kubecost_s3_exporter_cronjob_schedule" {
 }
 
 variable "kubecost_s3_exporter_ephemeral_volume_size" {
-  description = "(Optional) The ephemeral volume size for the Kubecost S3 Exporter pod"
-  type        = string
-  default     = "50Mi"
+  description = <<-EOF
+    (Optional) The ephemeral volume size for the Kubecost S3 Exporter pod.
+               Possible values: Size in the format of 'NMi', where N >= 1. For example, 10Mi, 50Mi, 100Mi, 150Mi.
+               Default value: 50Mi
+  EOF
+
+  type    = string
+  default = "50Mi"
 
   validation {
     condition     = can(regex("^[1-9]\\d?Mi.*$", var.kubecost_s3_exporter_ephemeral_volume_size))
@@ -54,9 +77,14 @@ variable "kubecost_s3_exporter_ephemeral_volume_size" {
 }
 
 variable "kubecost_api_endpoint" {
-  description = "(Optional) The Kubecost API endpoint in format of 'http://<name_or_ip>:<port>'"
-  type        = string
-  default     = "http://kubecost-cost-analyzer.kubecost:9090"
+  description = <<-EOF
+    (Optional) The Kubecost API endpoint in format of 'http://<name_or_ip>:<port>'.
+               Possible values: URI in the format of 'http://<name_or_ip>:[port]' or 'https://<name_or_ip>:[port]'.
+               Default value: http://kubecost-cost-analyzer.kubecost:9090
+  EOF
+
+  type    = string
+  default = "http://kubecost-cost-analyzer.kubecost:9090"
 
   validation {
     condition     = can(regex("^https?://.+$", var.kubecost_api_endpoint))
@@ -65,9 +93,14 @@ variable "kubecost_api_endpoint" {
 }
 
 variable "backfill_period_days" {
-  description = "(Optional) The number of days to check for backfilling"
-  type        = number
-  default     = 15
+  description = <<-EOF
+    (Optional) The number of days to check for backfilling.
+               Possible values: A positive integer equal or larger than 3.
+               Default value: 15
+  EOF
+
+  type    = number
+  default = 15
 
   validation {
     condition     = var.backfill_period_days >= 3
@@ -76,9 +109,14 @@ variable "backfill_period_days" {
 }
 
 variable "aggregation" {
-  description = "(Optional) The aggregation to use for returning the Kubecost Allocation API results"
-  type        = string
-  default     = "container"
+  description = <<-EOF
+    (Optional) The aggregation to use for returning the Kubecost Allocation API results.
+               Possible values: "container", "pod", "namespace", "controller", "controllerKind", "node" or "cluster".
+               Default value: container
+  EOF
+
+  type    = string
+  default = "container"
 
   validation {
     condition     = contains(["container", "pod", "namespace", "controller", "controllerKind", "node", "cluster"], var.aggregation)
@@ -87,9 +125,14 @@ variable "aggregation" {
 }
 
 variable "kubecost_allocation_api_paginate" {
-  description = "(Optional) Dictates whether to paginate using 1-hour time ranges (relevant for 1h step)"
-  type        = string
-  default     = "False"
+  description = <<-EOF
+    (Optional) Dictates whether to paginate using 1-hour time ranges (relevant for 1h step).
+               Possible values: "Yes", "No", "Y", "N", "True" or "False"
+               Default value: False
+  EOF
+
+  type    = string
+  default = "False"
 
   validation {
     condition     = can(regex("^(?i)(Yes|No|Y|N|True|False)$", var.kubecost_allocation_api_paginate))
@@ -98,9 +141,14 @@ variable "kubecost_allocation_api_paginate" {
 }
 
 variable "connection_timeout" {
-  description = "(Optional) The time (in seconds) to wait for TCP connection establishment"
-  type        = number
-  default     = 10
+  description = <<-EOF
+    (Optional) The time (in seconds) to wait for TCP connection establishment.
+               Possible values: A non-zero positive integer.
+               Default value: 10
+  EOF
+
+  type    = number
+  default = 10
 
   validation {
     condition     = var.connection_timeout > 0
@@ -109,9 +157,14 @@ variable "connection_timeout" {
 }
 
 variable "kubecost_allocation_api_read_timeout" {
-  description = "(Optional) The time (in seconds) to wait for the Kubecost Allocation API to send an HTTP response"
-  type        = number
-  default     = 60
+  description = <<-EOF
+    (Optional) The time (in seconds) to wait for the Kubecost Allocation API to send an HTTP response.
+               Possible values: A non-zero positive integer.
+               Default value: 60
+  EOF
+
+  type    = number
+  default = 60
 
   validation {
     condition     = var.kubecost_allocation_api_read_timeout > 0
@@ -120,9 +173,14 @@ variable "kubecost_allocation_api_read_timeout" {
 }
 
 variable "tls_verify" {
-  description = "(Optional) Dictates whether TLS certificate verification is done for HTTPS connections"
-  type        = string
-  default     = "True"
+  description = <<-EOF
+    (Optional) Dictates whether TLS certificate verification is done for HTTPS connections.
+               Possible values: "Yes", "No", "Y", "N", "True" or "False"
+               Default value: True
+  EOF
+
+  type    = string
+  default = "True"
 
   validation {
     condition     = can(regex("^(?i)(Yes|No|Y|N|True|False)$", var.tls_verify))
@@ -131,15 +189,26 @@ variable "tls_verify" {
 }
 
 variable "kubecost_ca_certificate_secrets" {
-  description = "(Optional) A list of AWS Secret Manager secrets created by the pipeline module"
-  type        = list(any)
-  default     = []
+  description = <<-EOF
+    (Optional) A list of AWS Secret Manager secrets created by the pipeline module.
+               Meant to only take a reference to the "kubecost_ca_cert_secret" output from the pipeline module.
+               Possible values: Only "module.pipeline.kubecost_ca_cert_secret" (without the double quotes).
+               Default value: empty list ([])
+  EOF
+
+  type    = list(any)
+  default = []
 }
 
 variable "kubecost_ca_certificate_secret_name" {
-  description = "(Optional) The AWS Secrets Manager secret name, for the CA certificate used for verifying Kubecost's server certificate when using HTTPS"
-  type        = string
-  default     = ""
+  description = <<-EOF
+    (Optional) The AWS Secrets Manager secret name, for the CA certificate used for verifying Kubecost's server certificate when using HTTPS.
+               Possible values: A valid AWS Secrets Manager secret name.
+               Default value: empty string ("")
+  EOF
+
+  type    = string
+  default = ""
 
   validation {
     condition     = can(regex("^$|^[\\w/+=.@-]{1,512}$", var.kubecost_ca_certificate_secret_name))
@@ -147,21 +216,15 @@ variable "kubecost_ca_certificate_secret_name" {
   }
 }
 
-variable "k8s_config_path" {
-  description = "(Optional) Full path to the K8s config file, to be used by Helm"
-  type        = string
-  default     = "~/.kube/config"
-
-  validation {
-    condition     = can(regex("^(~|\\/[ \\w.-]+)+$", var.k8s_config_path))
-    error_message = "The 'k8s_config_path' variable contains an invalid full file path"
-  }
-}
-
 variable "namespace" {
-  description = "(Optional) The namespace in which the Kubecost S3 Exporter pod and service account will be created"
-  type        = string
-  default     = "kubecost-s3-exporter"
+  description = <<-EOF
+    (Optional) The namespace in which the Kubecost S3 Exporter pod and service account will be created.
+               Possible values: A valid K8s namespace name.
+               Default value: kubecost-s3-exporter
+  EOF
+
+  type    = string
+  default = "kubecost-s3-exporter"
 
   validation {
     condition     = can(regex("^[a-z0-9]([-a-z0-9]{0,62}[a-z0-9])?$", var.namespace))
@@ -170,15 +233,25 @@ variable "namespace" {
 }
 
 variable "create_namespace" {
-  description = "(Optional) Dictates whether to create the namespace as part of the Helm Chart deployment"
-  type        = bool
-  default     = true
+  description = <<-EOF
+    (Optional) Dictates whether to create the namespace as part of the Helm Chart deployment.
+               Possible values: true or false.
+               Default value: true
+  EOF
+
+  type    = bool
+  default = true
 }
 
 variable "service_account" {
-  description = "(Optional) The service account for the Kubecost S3 Exporter pod"
-  type        = string
-  default     = "kubecost-s3-exporter"
+  description = <<-EOF
+    (Optional) The service account for the Kubecost S3 Exporter pod.
+               Possible values: A valid K8s service account name.
+               Default value: kubecost-s3-exporter
+  EOF
+
+  type    = string
+  default = "kubecost-s3-exporter"
 
   validation {
     condition     = can(regex("^[a-z0-9]([-a-z0-9]{0,252}[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?){0,252}$", var.service_account))
@@ -187,13 +260,23 @@ variable "service_account" {
 }
 
 variable "create_service_account" {
-  description = "(Optional) Dictates whether to create the service account as part of the Helm Chart deployment"
-  type        = bool
-  default     = true
+  description = <<-EOF
+    (Optional) Dictates whether to create the service account as part of the Helm Chart deployment.
+               Possible values: true or false.
+               Default value: true
+  EOF
+
+  type    = bool
+  default = true
 }
 
 variable "invoke_helm" {
-  description = "(Optional) Dictates whether to invoke Helm to deploy the K8s resources (the kubecost-s3-exporter CronJob and the Service Account)"
-  type        = bool
-  default     = true
+  description = <<-EOF
+    (Optional) Dictates whether to invoke Helm to deploy the K8s resources (the kubecost-s3-exporter CronJob and the Service Account).
+               Possible values: true or false.
+               Default value: true
+  EOF
+
+  type    = bool
+  default = true
 }
