cdd8ff109b007cf44d87d592376849576e48f739
Udi Dahan <udid@amazon.com>
2023-12-11 06:51:04 +0000

    - Added Glue outputs

--- terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
+++ terraform/kubecost_cid_terraform_module/modules/pipeline/outputs.tf
@@ -1,5 +1,15 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
+output "glue_database_name" {
+  description = "The AWS Glue Database name"
+  value       = aws_glue_catalog_database.kubecost_glue_db.name
+}
+
+output "glue_view_name" {
+  description = "The AWS Glue Table name for the Athena view"
+  value       = aws_glue_catalog_table.kubecost_glue_view.name
+}
+
 output "kubecost_ca_cert_secret" {
   description = "All AWS Secrets Manager Secrets"
   value       = aws_secretsmanager_secret.kubecost_ca_cert_secret
