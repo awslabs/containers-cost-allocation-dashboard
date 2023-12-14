fc543bfa2a51172fd7cc0a925db3c2ba990b4818
Udi Dahan <udid@amazon.com>
2023-12-14 12:41:54 +0000

    - Replaced references to common module with references to common variables module - Fixed typos in comments

--- terraform/cca_terraform_module/deploy/outputs.tf
+++ terraform/cca_terraform_module/deploy/outputs.tf
@@ -1,14 +1,14 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
-# Output for showing the distinct labels from all clusters, collected from the "clusters_metadata" common input
+# Output for showing the distinct labels from all clusters, collected from the "k8s_labels" common input
 output "labels" {
-  value       = length(module.common.k8s_labels) > 0 ? join(", ", distinct(module.common.k8s_labels)) : null
+  value       = length(module.common_variables.k8s_labels) > 0 ? join(", ", distinct(module.common_variables.k8s_labels)) : null
   description = "A list of the distinct labels of all clusters, that'll be added to the dataset"
 }
 
-# Output for showing the distinct annotations from all clusters, collected from the "clusters_metadata" common input
+# Output for showing the distinct annotations from all clusters, collected from the "k8s_annotations" common input
 output "annotations" {
-  value       = length(module.common.k8s_annotations) > 0 ? join(", ", distinct(module.common.k8s_annotations)) : null
+  value       = length(module.common_variables.k8s_annotations) > 0 ? join(", ", distinct(module.common_variables.k8s_annotations)) : null
   description = "A list of the distinct annotations of all clusters, that'll be added to the dataset"
 }
 
