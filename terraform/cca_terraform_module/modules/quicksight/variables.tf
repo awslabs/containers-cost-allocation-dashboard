8c4c9db8ffdacaad3c7273e17e5021ea1138c79d
Udi Dahan <udid@amazon.com>
2023-12-14 12:33:34 +0000

    - Added common module variables - Rearranged other variables into sections

--- terraform/cca_terraform_module/modules/quicksight/variables.tf
+++ terraform/cca_terraform_module/modules/quicksight/variables.tf
@@ -1,5 +1,69 @@
 # Copyright 2023 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 
+#                           #
+# Common Module's Variables #
+#                           #
+
+variable "k8s_labels" {
+  description = <<-EOF
+    (Optional) K8s labels common across all clusters, that you wish to include in the dataset.
+               Meant to only take a reference to the "k8s_labels" output from the common module.
+               Possible values: Only "module.common.k8s_labels" (without the double quotes).
+               Default value: empty list ([]).
+  EOF
+
+  type    = list(string)
+  default = []
+
+  validation {
+    condition = (
+      length([
+        for k8s_label in var.k8s_labels : k8s_label
+        if can(regex("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9-]*[A-Za-z0-9])\\/[a-zA-Z0-9][-\\w.]{0,61}[a-zA-Z0-9]$|^[a-zA-Z0-9][-\\w.]{0,61}[a-zA-Z0-9]$", k8s_label))
+      ]) == length(var.k8s_labels)
+    )
+    error_message = "At least one of the items the 'k8s_labels' list, contains an invalid K8s label key"
+  }
+}
+
+variable "k8s_annotations" {
+  description = <<-EOF
+    (Optional) K8s annotations common across all clusters, that you wish to include in the dataset.
+               Meant to only take a reference to the "k8s_annotations" output from the common module.
+               Possible values: Only "module.common.k8s_annotations" (without the double quotes).
+               Default value: empty list ([]).
+  EOF
+
+  type    = list(string)
+  default = []
+
+  validation {
+    condition = (
+      length([
+        for k8s_annotation in var.k8s_annotations : k8s_annotation
+        if can(regex("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9-]*[A-Za-z0-9])\\/[a-zA-Z0-9][-\\w.]{0,61}[a-zA-Z0-9]$|^[a-zA-Z0-9][-\\w.]{0,61}[a-zA-Z0-9]$", k8s_annotation))
+      ]) == length(var.k8s_annotations)
+    )
+    error_message = "At least one of the items the 'k8s_annotations' list, contains an invalid K8s annotation key"
+  }
+}
+
+variable "aws_common_tags" {
+  description = <<-EOF
+    (Optional) Common AWS tags to be used on all AWS resources created by Terraform.
+               Meant to only take a reference to the "aws_common_tags" output from the common module.
+               Possible values: Only "module.common.aws_common_tags" (without the double quotes).
+               Default value: empty map ({}).
+  EOF
+
+  type    = map(any)
+  default = {}
+}
+
+#                           #
+# Pipeline Module Variables #
+#                           #
+
 variable "glue_database_name" {
   description = <<-EOF
     (Optional) The AWS Glue database name.
@@ -31,6 +95,10 @@ variable "glue_view_name" {
     error_message = "The 'glue_view_name' variable contains an invalid AWS Glue Table name"
   }
 }
+
+#                         #
+# This Module's Variables #
+#                         #
 
 variable "athena_workgroup_configuration" {
   description = <<-EOF
