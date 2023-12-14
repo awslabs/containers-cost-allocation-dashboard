f4ede8958d80785baee93824c9fd5e99e338f7f9
Udi Dahan <udid@amazon.com>
2023-12-14 07:41:08 +0000

    - Removed bucket_name, static_columns and partition_keys outputs

--- terraform/cca_terraform_module/modules/common_variables/outputs.tf
+++ terraform/cca_terraform_module/modules/common_variables/outputs.tf
@@ -5,11 +5,6 @@ output "bucket_arn" {
   value       = var.bucket_arn
 }
 
-output "bucket_name" {
-  description = "The name of the S3 Bucket to which the Kubecost data will be uploaded"
-  value       = local.bucket_name
-}
-
 output "k8s_labels" {
   description = "K8s labels common across all clusters, that you wish to include in the dataset"
   value       = var.k8s_labels
@@ -23,14 +18,4 @@ output "k8s_annotations" {
 output "aws_common_tags" {
   description = "Common AWS tags to be used on all AWS resources created by Terraform"
   value       = var.aws_common_tags
-}
-
-output "static_columns" {
-  description = "A list of the schema's static columns, mapped to their AWS Glue Table types and QuickSight Dataset types"
-  value       = local.static_columns
-}
-
-output "partition_keys" {
-  description = "A list of the schema's partition keys, mapped to their AWS Glue Table types and QuickSight Dataset types"
-  value       = local.partition_keys
 }
