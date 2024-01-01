80d67d5eaabc3358250f7694bdee4f6720199412
Udi Dahan <udid@amazon.com>
2024-01-01 17:34:27 +0200

    Changed S3 bucket name in main.tf example

--- terraform/cca_terraform_module/examples/root_module/main.tf
+++ terraform/cca_terraform_module/examples/root_module/main.tf
@@ -32,7 +32,7 @@ terraform {
 module "common_variables" {
   source = "./modules/common_variables"
 
-  bucket_arn      = "arn:aws:s3:::udid-data-collection-kubecost-data"
+  bucket_arn      = "arn:aws:s3:::bucket-name"
   k8s_labels      = ["app", "chart", "component", "app.kubernetes.io/version", "app.kubernetes.io/managed_by", "app.kubernetes.io/part_of"]
   k8s_annotations = ["kubernetes.io/psp", "eks.amazonaws.com/compute_type"]
   aws_common_tags = {
